0e87202f7728e141e6f0e881366f4ecf148d921a
DhineshODI <dhineshkodi2024@gmail.com>
2025-10-24 10:32:01 +0530

    24-10-2025-morning-update(Project-Detail-page

--- src/Components/Project-Detail-Components/ProjectDetailSectionContent.jsx
+++ src/Components/Project-Detail-Components/ProjectDetailSectionContent.jsx
@@ -1,12 +1,27 @@
 export default function ProjectDetailSectionContent() {
   return (
     <>
-      <div>
+      <div className="mainContainerDetailProject">
         <div className="ProjectDetailSectionContentSection">
           <img
+            className="projectDetailBannerIMageMain"
             src="/images/project-detail/project-detail-project-banner.jpg"
             alt=""
           />
+
+          <div>
+            <div className="projectDetailProjectName">
+              <img src="/images/project-detail/builind-icon-pink.svg" alt="" />
+              <div className="projectNameDetailPage">
+                <h5>Spectra</h5>
+                <p>Keelkattalai, Chennai</p>
+              </div>
+              <div className="projectNameDetailPage">
+                <h5>Spectra</h5>
+                <p>Keelkattalai, Chennai</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
