5530aac42f25cd94abf4569d61b348a4e7698f2b
DhineshODI <dhineshkodi2024@gmail.com>
2025-11-04 10:20:10 +0530

    04-11-2025-update

--- src/Components/Project-Detail-Components/ProjectDetailSectionContent.jsx
+++ src/Components/Project-Detail-Components/ProjectDetailSectionContent.jsx
@@ -9,16 +9,50 @@ export default function ProjectDetailSectionContent() {
             alt=""
           />
 
-          <div>
+          <div className="spectraprojectdetailssection">
             <div className="projectDetailProjectName">
-              <img src="/images/project-detail/builind-icon-pink.svg" alt="" />
-              <div className="projectNameDetailPage">
-                <h5>Spectra</h5>
-                <p>Keelkattalai, Chennai</p>
+              <div className="mainconatinerprojectflex">
+                <img
+                  src="/images/project-detail/builind-icon-pink.svg"
+                  alt=""
+                />
+                <div className="projectNameDetailPage">
+                  <h5>Spectra</h5>
+                  <p>Keelkattalai, Chennai</p>
+                </div>
               </div>
-              <div className="projectNameDetailPage">
-                <h5>Spectra</h5>
-                <p>Keelkattalai, Chennai</p>
+
+              <div className="projectNameDetailPage projectdetailflex">
+                <img
+                  src="/images/project-detail/builind-icon-pink.svg"
+                  alt=""
+                />
+
+                <div className="flex items-end gap-2">
+                <h5>2 & 3</h5>
+                <p>BHK Apartment</p>
+                </div>
+
+              </div>
+            </div>
+
+            <div className="noffloorswidth">
+              <div>
+                <p className="nooffloors">
+                  No of Floors :<span> Stilt + 5 Floors</span>{" "}
+                </p>
+                <p className="nooffloors">
+                  Land Extent :<span> 22,058 Sq.ft.</span>{" "}
+                </p>
+                <p className="nooffloors">
+                  Total :<span> Units 41</span>{" "}
+                </p>
+                <p className="nooffloors">
+                  3 BHK :<span> 1481 - 1608 Sq.ft.</span>{" "}
+                </p>
+                <p className="nooffloors">
+                  2 BHK : <span> 1069 -1159 Sq.ft.</span>{" "}
+                </p>
               </div>
             </div>
           </div>
