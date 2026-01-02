1924a7de439879766cecece32c61bde07d0d2692
DhineshODI <dhineshkodi2024@gmail.com>
2026-01-02 13:06:35 +0530

    02-01-2026

--- src/Pages/NriPage.jsx
+++ src/Pages/NriPage.jsx
@@ -31,32 +31,6 @@ export default function NriPage() {
         />
 
         {/* Schema Markup */}
-        <script type="application/ld+json">
-          {`
-                {
-                  "@context": "https://schema.org",
-                  "@type": "RealEstateAgent",
-                  "name": "NRI",
-                  "image": "https://keystonepromoters.com/images/keystone-detailpage-logo.jpg",
-                  "@id": "",
-                  "url": "https://keystonepromoters.com/nri",
-                  "telephone": "99400 08855",
-                  "address": {
-                    "@type": "PostalAddress",
-                    "streetAddress": "Door No.1, GF-A, Amace Ganga Sarojini Street, Mahalingapuram, Nungambakkam,",
-                    "addressLocality": "Chennai",
-                    "postalCode": "600034",
-                    "addressCountry": "IN"
-                  } ,
-                  "sameAs": [
-                    "https://www.facebook.com/keystonechennai/",
-                    "https://www.instagram.com/keystone_promoters/",
-                    "https://www.youtube.com/@KeystonePromoters",
-                    "https://www.linkedin.com/company/keystone-promoters-pvt-ltd---india?originalSubdomain=in"
-                  ] 
-                }
-              `}
-        </script>
       </Helmet>
 
       <div className="">
--- src/Pages/ProjectDetailMainPage.jsx
+++ src/Pages/ProjectDetailMainPage.jsx
@@ -29,32 +29,6 @@ export default function ProjectDetailMainPage() {
         />
 
         {/* Schema Markup */}
-        <script type="application/ld+json">
-          {`
-              {
-                "@context": "https://schema.org",
-                "@type": "RealEstateAgent",
-                "name": "Spectra",
-                "image": "https://keystonepromoters.com/images/keystone-detailpage-logo.jpg",
-                "@id": "",
-                "url": "https://keystonepromoters.com/ongoing-projects/spectra",
-                "telephone": "9940008855",
-                "address": {
-                  "@type": "PostalAddress",
-                  "streetAddress": "Door No.1, GF-A, Amace Ganga Sarojini Street, Mahalingapuram, Nungambakkam,",
-                  "addressLocality": "Chennai",
-                  "postalCode": "600034",
-                  "addressCountry": "IN"
-                } ,
-                "sameAs": [
-                  "https://www.instagram.com/keystone_promoters/",
-                  "https://www.facebook.com/keystonechennai/",
-                  "https://www.youtube.com/@KeystonePromoters",
-                  "https://www.linkedin.com/company/keystone-promoters-pvt-ltd---india/about/"
-                ] 
-              }
-          `}
-        </script>
       </Helmet>
 
       <div className="projectDetailPage">
